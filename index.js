4c93b801ab8a9dfb7cb94dbde278905d8a155757
sabrinal <s21810001@student.unklab.ac.id>
2020-10-16 17:50:51 +0800

    Update2

--- index.js
+++ index.js
@@ -13,3 +13,44 @@ btnAdd.addEventListener('click', () => {
         btnAdd.textContent = 'Show Form Add Student';
     }
 }) 
+form.addEventListener('submit', (a) => {
+    a.preventDefault();
+    const input = document.querySelector('[name = "id"]');
+    const input1 = document.querySelector('[name = "fullname"]');
+    const gender = [document.querySelector('.g1'),document.querySelector('.g2')]
+  let male_female = gender.reduce(gd=>{
+    if (gd.checked) {
+      return gd.value;
+    }
+  })
+    const faculty = document.querySelector('.select');    
+    const btnadd = document.querySelector('.tombol');
+    // btnadd.addEventListener('click', (b) => {
+   // })
+ 
+
+   const element = document.createElement('tr');
+   const element1 = document.createElement('td');
+   const element2 = document.createElement('td');
+   const element3 = document.createElement('td');
+   const el = document.querySelector('#students');
+   element1.appendChild(input);
+   element2.appendChild(input1);
+   element3.appendChild(male_female);
+   element.appendChild(element1);
+   element.appendChild(element2);
+   element.appendChild(element3);
+   el.appendChild(element);
+   
+
+
+
+
+
+
+
+   console.log(input.value);
+   console.log(input1.value);
+   
+
+})
